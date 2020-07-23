745c2e4ae12a166e552486030111dde813535c7c
Robert Hümmer <robert.huem@gmail.com>
2020-07-23 14:01:22 +0200

    added properties for VSC to settings.json so that unneccessary tabs and spaces are removed by saving

--- fur_pattern_generator.py
+++ fur_pattern_generator.py
@@ -14,39 +14,39 @@ class Cells:
 	def __init__(self, image):
 		self.width  = image.width()
 		self.height = image.height()
-		
-		self.visited = [[0 
-			for i in range(self.width)] 
+
+		self.visited = [[0
+			for i in range(self.width)]
 			for j in range(self.height)]
-			
-		self.cells = [["D" if image.getPixel_HSV(i,j)[2] < 0.2 else 'U' 
-			for i in range(self.width)] 
+
+		self.cells = [["D" if image.getPixel_HSV(i,j)[2] < 0.2 else 'U'
+			for i in range(self.width)]
 			for j in range(self.height)]
 
 	def increaseVisits(self, u, v):
 		self.visited[v][u] += 1
-		
+
 	def setVisited(self, u, v):
 		self.visited[v][u] = 1
-		
+
 	def gotVisited(self, u, v):
 		if self.visited[v][u] > 0:
 			return True
 		else:
 			return False
-	
+
 	def resetVisited(self):
-		self.visited = [[0 for i in range(self.width)] 
+		self.visited = [[0 for i in range(self.width)]
 			for j in range(self.height)]
-		
+
 	def set(self, u, v, state):
 		self.cells[v][u] = state
-		
+
 	def get(self, u, v):
 		return self.cells[v][u]
-	
+
 	def reset(self):
-		self.cells = [[0 for i in range(self.width)] 
+		self.cells = [[0 for i in range(self.width)]
 			for j in range(self.height)]
 
 	def print(self):
@@ -57,7 +57,7 @@ class Cells:
 				print('{:1}'.format(val), end='')
 			print(']')
 		print()
-		
+
 	def printVisits(self):
 		print()
 		for row in self.visited[::-1]:
@@ -75,16 +75,16 @@ class Image:
 	"""
 	def __init__(self, image_file):
 		self.img = bpy.data.images[image_file]
-		
+
 	def width(self):
 		return self.img.size[0]
-	
+
 	def height(self):
 		return self.img.size[1]
-		
+
 	def isValidCoord(self, x, y):
 		"""
-		Returns 'True', if the supplied coordinates 
+		Returns 'True', if the supplied coordinates
 		are in the Image. Otherwhise returns 'False'.
 		"""
 		if x >= 0 and x < self.img.size[0] and \
@@ -101,20 +101,20 @@ class Image:
 			index = (y * self.width() + x) * 4
 			cell = self.img.pixels[index:index+4]
 			return cell
-		return [0,0,0,0] 
+		return [0,0,0,0]
 
 	def getPixel_HSV(self, x, y):
 		"""
 		Gets the pixel's color at 'x' 'y' as HSV.
 		If no valid coords 'x' and 'y' are supplied, then [0,0,0,0] is returned.
 		"""
-		rgba = self.getPixel_RGBA(x, y) 
+		rgba = self.getPixel_RGBA(x, y)
 		hsv  = colorsys.rgb_to_hsv(rgba[0], rgba[1], rgba[2])
 		return hsv
-	 
+
 	def setPixel_RGBA(self, x, y, RGBA):
-		""" 
-		Sets the pixel's color at 'x' 'y' with RGBA. 
+		"""
+		Sets the pixel's color at 'x' 'y' with RGBA.
 		If no valid coords 'x' and 'y' are supplied, then only 'False' is returned.
 		"""
 		if self.isValidCoord(x, y):
@@ -122,9 +122,9 @@ class Image:
 			self.img.pixels[index:index+4] = RGBA
 			return True
 		return False
-	 
+
 	def setPixel_HSV(self, x, y, HSV):
-		""" 
+		"""
 		Sets the pixel's color at 'x' 'y' with HSV.
 		If no valid coords 'x' and 'y' are supplied, then only 'False' is returned.
 		"""
@@ -144,19 +144,19 @@ def countDCells(image, cells, x, y, mh_radius):
 	@param mh_radius	manhattan radius
 	"""
 	cellCount = 0
-	
+
 	# cell got already visited
 	if cells.gotVisited(x, y) == True:
 		return 0
-	
+
 	# is valid coords:
 	if image.isValidCoord(x, y) == False:
-		return 0	
-	
+		return 0
+
 	# recursion break-condition
 	if mh_radius < 0:
 		return 0
-		
+
 	# get pixel
 	cellInfo = cells.get(x, y)
 	if cellInfo == "D":
@@ -164,28 +164,28 @@ def countDCells(image, cells, x, y, mh_radius):
 		cellCount += 1
 #	else:
 #		print("U-Cell at : (", x, ", ", y, ")", " inside r: ", mh_radius)
-		
+
 	# set cell to visited
-	cells.increaseVisits(x, y) 
-	
+	cells.increaseVisits(x, y)
+
 	# print("hsv: ", HSV)
 	stillToVisit = []
 	lookup = [ [1,0], [-1,0], [0,1], [0,-1] ]
-	
+
 	if mh_radius == 0:
 		return cellCount
-	
-	for l in lookup:		
+
+	for l in lookup:
 		loc_x = x + l[0]
 		loc_y = y + l[1]
 #		print("look at : (", loc_x, ", ", loc_y, ")")
-		
+
 		# stop at border; no wrap-around!!!
 		# and if cell is still unvisited
 		if  image.isValidCoord(loc_x, loc_y) and \
 			cells.gotVisited(loc_x, loc_y) == False:
 #			print("investigate at : (", loc_x, ", ", loc_y, ")")
-			
+
 			# remember still to visit pixels
 			stillToVisit.append([loc_x, loc_y])
 #			cellCount += countDCells(img, cells, loc_x, loc_y, mh_radius-1)
@@ -202,7 +202,7 @@ def countDCells(image, cells, x, y, mh_radius):
 def CA_young(image):
 	width  = image.width()
 	height = image.height()
-	
+
 	# we need to know the image dimensions
 	print ("Image size: ", width, " x ", height)
 
@@ -220,14 +220,14 @@ def CA_young(image):
 #			cells.printVisits()
 			w=0.69
 			Disc = AD - w * ID
-			
+
 			if (Disc > 0):
 #				print("### formula: ", Disc, " = ", AD, " - ", w, " * ", ID)
 				cells.set(u, v, "D")
 			elif (Disc < 0):
 #				print("### formula: ", Disc, " = ", AD, " - ", w, " * ", ID)
 				cells.set(u, v, "U")
-					
+
 	print("2nd pass start")
 	# 2nd pass : apply cells to image:
 	for u in range(height):
@@ -236,7 +236,7 @@ def CA_young(image):
 				image.setPixel_HSV(u, v, [0,0,0])
 			elif cells.get(u, v) == "U":
 				image.setPixel_HSV(u, v, [0.05,0.8,1.0] )
-			
+
 #	cells.print()
 
 
@@ -249,11 +249,10 @@ if __name__ == "__main__":
 	for u in range(image.height()):
 		for v in range(image.width()):
 			image.setPixel_HSV(u, v, [0,0,random.randint(0,1)])
-				
+
 	CA_young(image)
-	
-	
-	
-	
+
+
+
+
 	print("FINISHED")
-	
