8f94f90e0f57fdeac8c8b6ef9f0d9337693a3ae3
RobertHue <robert.huem@gmail.com>
2023-05-06 22:59:29 +0200

    refactored fur_pattern_generator

--- fur_pattern_generator/addon/fur_pattern_generator.py
+++ fur_pattern_generator/addon/fur_pattern_generator.py
@@ -1,17 +1,18 @@
-import bpy
+# Python Module Index
 import colorsys
-
-import numpy as np
 import random
-from multiprocessing import Pool
-from multiprocessing import cpu_count
-from cv2 import cv2
 
+# Blender
+import bpy
+
+# 3rd Party
+import cv2
+import numpy as np
 
 
 class Cells:
 	"""
-	This class defines the cells of the CA
+	This class defines the cells of the Cellular Automata (CA).
 	"""
 
 	def __init__(self, image, color_D):
@@ -38,7 +39,6 @@ class Cells:
 
 	def getDisc(self, u, v):
 		return self.disc[v][u]
-
 
 	def increaseVisits(self, u, v):
 		self.visited[v][u] += 1
@@ -107,7 +107,6 @@ class Cells:
 		print()
 
 
-
 class Image:
 	"""
 	Pass an image file loaded into Blender when creating an Image-object.
@@ -123,20 +122,12 @@ class Image:
 
 	def export_CV(self):
 		pixelList = list(self._img.pixels)
-		# print("pixelList : ", pixelList)
-		# print("len(pixelList): ", len(pixelList))
 		rgbPixels = [x for i, x in enumerate(pixelList) if (i+1)%4 != 0]
 		a = np.array(rgbPixels)
-		# print("a : ", a)
-		# print("len(a): ", len(a))
 		b = np.reshape(a, (self.height(), self.width(), 3))
-		# print("b : ", b)
-		# print("len(b): ", len(b))
 		rgba = np.ones((self.height(), self.width(), 3), dtype=np.uint8)
 		rgba[:,:,:] = np.uint8(b) * 255
 		cv_image = np.flip(rgba, axis=[0, 2])
-		# print("cv_image: ", cv_image)
-		# print("len(cv_image): ", len(cv_image))
 		return np.float32(cv_image)
 
 	def import_CV(self, cv_image):
@@ -171,9 +162,11 @@ class Image:
 		Gets the pixel's color at 'x' 'y' as HSV.
 		If no valid coords 'x' and 'y' are supplied, then [0,0,0,0] is returned.
 		"""
-		rgba = self.getPixel_RGBA(x, y)
-		hsv  = colorsys.rgb_to_hsv(rgba[0], rgba[1], rgba[2])
-		return hsv
+		if self.isValidCoord(x, y):
+			rgba = self.getPixel_RGBA(x, y)
+			hsv  = colorsys.rgb_to_hsv(rgba[0], rgba[1], rgba[2])
+			return hsv
+		return [0,0,0,0]
 
 	def setPixel_RGBA(self, x, y, RGBA):
 		"""
@@ -191,11 +184,12 @@ class Image:
 		Sets the pixel's color at 'x' 'y' with HSV.
 		If no valid coords 'x' and 'y' are supplied, then only 'False' is returned.
 		"""
-		rgb  = colorsys.hsv_to_rgb(HSV[0], HSV[1], HSV[2])
-		rgba = list(rgb)
-		rgba.append(1)
-		return self.setPixel_RGBA(x, y, rgba)
-
+		if self.isValidCoord(x, y):
+			rgb  = colorsys.hsv_to_rgb(HSV[0], HSV[1], HSV[2])
+			rgba = list(rgb)
+			rgba.append(1)
+			return self.setPixel_RGBA(x, y, rgba)
+		return False
 
 
 def drawCircle(image, color, x, y, radius):
@@ -212,7 +206,6 @@ def drawCircle(image, color, x, y, radius):
 
 	# write back image with circle into image
 	image.import_CV(new_img)
-
 
 
 def getCircularNeighborhood(image, x, y, radius):
@@ -234,26 +227,23 @@ def getCircularNeighborhood(image, x, y, radius):
 	return n
 
 
+def getMooreNeighborhood(image, cells, source_pixel, radius):
+	"""Gets the Moore Neighborhood as a list of pixels including the source pixel."""
+	moore_lookup = [[1,0],[-1,0],[0,1],[0,-1], [1,1], [-1,-1], [1,-1], [1,-1]]
+	result_n = []  # the neighboorhood result
+	queue = []  # create a queue for BFS
+	cells.resetVisited()  # to memorize that the cell has been visited once
 
-def getManhattanNeighborhood(image, cells, x, y, radius):
-	lookup = [[1,0],[-1,0],[0,1],[0,-1], [1,1], [-1,-1], [1,-1], [1,-1]] # moore-neighboorhood
-	# lookup = [[1,0],[-1,0],[0,1],[0,-1]]
-	n = []
+	# Are coords inside the image; hence valid?
+	x = source_pixel[0]
+	y = source_pixel[1]
+	if not image.isValidCoord(x, y):
+		return neighborhood
 
-	# mark all the pixels as not visited
-	cells.resetVisited()
-
-	# create a queue for BFS
-	queue = []
-
-	# are valid coords inside the image:
-	if image.isValidCoord(x, y) == False:
-		return n
-
-	# Mark the source node as
+	# Mark the source pixel as
 	# visited and enqueue it
-	coords = [x,y]
-	n = [coords]
+	coords = source_pixel
+	result_n = [coords]
 	cells.increaseVisits(x, y)
 	queue.append(coords)
 
@@ -263,40 +253,37 @@ def getManhattanNeighborhood(image, cells, x, y, radius):
 		while level_size > 0:
 			level_size -= 1
 
-			# dequeue a vertex from queue and print it
-			s = queue.pop(0)
-			n += [s]
+			# dequeue a pixel as src_pixel from queue
+			src_pixel = queue.pop(0)
+			result_n += [src_pixel]
 
 			if not queue:
 				radius -= 1
 
-			# get all adjacent pixels of dequeued pixel s
-			# if a adjacen has not been visited, then mark it visited and enqueue it
-			for l in lookup:
-				new_x = s[0] + l[0]
-				new_y = s[1] + l[1]
+			# get all adjacent pixels of that dequeued src_pixel
+			# if a adjacent has not been visited, then mark it visited and enqueue it
+			for lookup in moore_lookup:
+				new_x = src_pixel[0] + lookup[0]
+				new_y = src_pixel[1] + lookup[1]
 
-				# are valid coords inside the image:
-				if image.isValidCoord(new_x, new_y) == False:
+				# Are valid coords inside the image:
+				if not image.isValidCoord(new_x, new_y):
 					continue
 
 				# cell got already visited
-				if cells.gotVisited(new_x, new_y) == True:
+				if cells.gotVisited(new_x, new_y):
 					continue
 
 				coords = [new_x, new_y]
 				queue.append(coords)
 				cells.setVisited(new_x, new_y)
-				# cells.printVisits()
-				# print()
 		radius -= 1
-	return n
+	return result_n
 
 
-
-def countDCells(image, cells, x, y, radius):
+def countDCells(image: Image, cells: Cells, pos, r):
 	"""
-	Counts the D cells in a given Radius.
+	Counts the D cells at position pos in a given radius r.
 	@param cells 	empty cell-set that adds additional info to the image
 	@param x 		x pos of the center
 	@param y 		y pos of the center
@@ -304,14 +291,12 @@ def countDCells(image, cells, x, y, radius):
 	"""
 	cellCount = 0
 
-	regionList = getManhattanNeighborhood(image, cells, x, y, radius)
-	# print("regionList: ", regionList)
+	regionList = getMooreNeighborhood(image, cells, pos, r)
 
-
-	for r in regionList:
+	for region in regionList:
 		# update coords:
-		x = r[0]
-		y = r[1]
+		x = region[0]
+		y = region[1]
 
 		# get pixel
 		cellInfo = cells.get(x, y)
@@ -343,7 +328,6 @@ def generate_random(image, rgb_color_D, rgb_color_U):
 				image.setPixel_HSV(u, v, color_U)
 
 
-
 def CA_young(image, r_activator, r_inhibitor, w, rgb_color_D, rgb_color_U):
 	# we need to know the image dimensions
 	width  = image.width()
@@ -357,32 +341,24 @@ def CA_young(image, r_activator, r_inhibitor, w, rgb_color_D, rgb_color_U):
 	print("color_U: ", color_U)
 
 	cells = Cells(image, color_D)
-	# cells.printDiscs()
-	# cells.print()
 	# 1st pass : calculate cells Disc and apply cells-set
 	print("1st pass start")
 	for u in range(height):
 		for v in range(width):
 			cells.resetVisited()
-			AD = countDCells(image, cells, u, v, r_activator)
+			AD = countDCells(image, cells, [u, v], r_activator)
 			# cells.printVisits()
 
 			cells.resetVisited()
-			ID = countDCells(image, cells, u, v, r_inhibitor) - AD
+			ID = countDCells(image, cells, [u, v], r_inhibitor) - AD
 			# cells.printVisits()
 
 			# This computation happens to all cells at the same time,
 			# therefore we must defer the setting of the color to a 2nd step.
 			disc = AD - w * ID
 			cells.setDisc(u, v, disc)
-	# debug-output:
-	# print("disc = AD - w * ID")
-	# print(disc, " = ", AD, " - ", w, " * ", ID)
-	# print(disc, " = ", AD, " - ", w * ID)
-	# print(disc, " = ", AD - w * ID)
 	cells.printDiscs()
 	cells.print()
-	#cells.printVisits()
 	print("2nd pass start")
 	# 2nd pass : apply cells to image:
 	for u in range(height):
