384c626eab16e49abfd2fbc30eab7e88b168a2e3
RobertHue <robert.huem@gmail.com>
2023-08-06 22:50:02 +0200

    fixed imports of packages

---
+++ src/fpg/__init__.py
@@ -0,0 +1,21 @@
+from .generator import Cells
+from .generator import Image
+from .generator import draw_circle
+from .generator import get_circular_neighborhood
+from .generator import get_moore_neighborhood
+from .generator import count_d_cells
+from .generator import rgb2hsv
+from .generator import generate_random
+from .generator import cellular_automata
+
+__all__ = [
+    "Cells",
+    "Image",
+    "draw_circle",
+    "get_circular_neighborhood",
+    "get_moore_neighborhood",
+    "count_d_cells",
+    "rgb2hsv",
+    "generate_random",
+    "cellular_automata",
+]
