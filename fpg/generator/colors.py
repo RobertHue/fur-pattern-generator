f864ed8a2f064be449fbecb41fcc15fe3616ae5a
RobertHue <robert.huem@gmail.com>
2023-08-12 16:08:57 +0200

    fixed colors.py

--- fpg/generator/colors.py
+++ fpg/generator/colors.py
@@ -1,4 +1,5 @@
 from colorsys import rgb_to_hsv
+
 from typing import NamedTuple
 
 
@@ -29,12 +30,11 @@ T_RGB = tuple[int, int, int]
 T_RGBA = tuple[int, int, int, int]
 
 # Constants
-RGBA_COLOR_D = RGB_Color(r=11, g=13, b=15, a=255)  # BLACK
-RGBA_COLOR_U = RGB_Color(r=255, g=111, b=222, a=255)  # WHITE
-
-color_tuple_d = (*rgb_to_hsv(*RGBA_COLOR_D[:3]), RGBA_COLOR_D[3])
-color_tuple_u = (*rgb_to_hsv(*RGBA_COLOR_U[:3]), RGBA_COLOR_D[3])
-inter_d = (int(value * 255) for value in color_tuple_d)
-inter_u = (int(value * 255) for value in color_tuple_u)
-HSV_COLOR_D = HSV_Color(*inter_d)
-HSV_COLOR_U = HSV_Color(*inter_u)
+RGBA_COLOR_D = RGB_Color(r=255, g=0, b=0, a=255)  # BLACK
+RGBA_COLOR_U = RGB_Color(r=0, g=0, b=255, a=255)  # WHITE
+RGBA_COLOR_ND = [int(value / 255) for value in RGBA_COLOR_D]  # N = normalized
+RGBA_COLOR_NU = [int(value / 255) for value in RGBA_COLOR_U]  # N = normalized
+color_tuple_d = (*rgb_to_hsv(*RGBA_COLOR_ND[:3]), RGBA_COLOR_ND[3])
+color_tuple_u = (*rgb_to_hsv(*RGBA_COLOR_NU[:3]), RGBA_COLOR_NU[3])
+HSV_COLOR_D = HSV_Color(*color_tuple_d)
+HSV_COLOR_U = HSV_Color(*color_tuple_u)
