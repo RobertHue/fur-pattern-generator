a82e55dbca6b964b77ac0bf1bacd0b5a97dc31ae
RobertHue <robert.huem@gmail.com>
2023-08-24 00:02:07 +0200

    modules needs to export RGB_Color

--- fpg/generator/__init__.py
+++ fpg/generator/__init__.py
@@ -1,5 +1,6 @@
 from .cells import Cells
 from .colors import NP_RGBA_DTYPE
+from .colors import RGB_Color
 from .image import Image
 from .image import flatlist_to_numpy
 from .image import numpy_to_flatlist
@@ -9,6 +10,7 @@ __all__ = [
     "Cells",
     "Image",
     "NP_RGBA_DTYPE",
+    "RGB_Color",
     "flatlist_to_numpy",
     "numpy_to_flatlist",
 ]
